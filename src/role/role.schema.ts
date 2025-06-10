0949baa280e7dbca7426823b8d1927baff5896b4
Yesarib <yesaribaris23@gmail.com>
2025-06-10 17:38:37 +0300

    refactor: update role schema to use Types.ObjectId for privileges

--- src/role/role.schema.ts
+++ src/role/role.schema.ts
@@ -1,7 +1,6 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { Document, Schema as MongooseSchema } from 'mongoose'
+import { Document, Types } from 'mongoose'
 import { Role } from "./interface/role.interface";
-import { PrivilegeModel } from "src/privileges/privilege.schema";
 
 export type RoleDocument = Role & Document
 
@@ -13,8 +12,8 @@ export class RoleModel extends Document implements Role {
     })
     name: string;
 
-    @Prop({ type: [{ type: MongooseSchema.Types.ObjectId, ref: 'PrivilegeModel' }] })
-    privileges: PrivilegeModel[];
+    @Prop({ type: [{ type: Types.ObjectId, ref: 'PrivilegeModel' }] })
+    privileges: Types.ObjectId[];
 }
 
 export const RoleSchema = SchemaFactory.createForClass(RoleModel);
